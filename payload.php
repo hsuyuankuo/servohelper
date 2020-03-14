8ed240d5bd707348457f857e5f7fbea2c5fa1187
neil <hsuyuankuko@gmail.com>
2020-03-14 20:49:30 +0800

    update payload.php

--- payload.php
+++ payload.php
@@ -4,10 +4,13 @@ $body = file_get_contents('php://input');
 $sig_check = 'sha1=' . hash_hmac('sha1', $body, 'corona_age');
 $headers = apache_request_headers();
 
+
+
 if (hash_equals($sig_check, $headers['X-Hub-Signature']))
 {
-    $output = shell_exec( 'sh /var/web/servohelper/pull_servohelper.sh' );
-    echo $output;	
+    echo "rsa checked\n";
+    $output = shell_exec( 'sudo sh /var/web/servohelper/pull_servohelper.sh' );
+    echo $output;
 }
 else
 {
